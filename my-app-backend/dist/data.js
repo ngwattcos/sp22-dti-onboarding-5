14d86749e993f2edf526cb7913b8fffe56a2b46f
Scott Wang <curiosity142000@gmail.com>
2022-04-12 23:49:12 -0400

    remove builds

--- my-app-backend/dist/data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createUser = void 0;
-const createUser = (name, age) => {
-    return { name, age };
-};
-exports.createUser = createUser;
-//# sourceMappingURL=data.js.map
